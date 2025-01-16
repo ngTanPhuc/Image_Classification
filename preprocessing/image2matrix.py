9c12f86949f6e67c65da50125d24880ed8487fd5
Nguyễn Tấn Phúc <99126475+JackWolffang@users.noreply.github.com>
2025-01-16 14:11:12 +0700

    1st version of image2matrix.py

--- preprocessing/image2matrix.py
+++ preprocessing/image2matrix.py
@@ -1,6 +1,6 @@
 # This is the 1st step which changes the images into matrices of integers (grayscale or RGB)
 # This file contains a function which return the matrix of an input image. The function will then
-# be called in the dataset.py
+# be called each time we fetch an image into the dataset of the TensorDataset
 
 # Grayscale: 2D matrix/tensor with each element represent the brightness (from 0 to 255 or from black to white)
 #   pros: smaller in size, reduce the amount of data to make the training faster, useful when color is not important
@@ -9,3 +9,15 @@
 # dimension represent the color channel (R, G, B) which stands for red, green and blue
 # You may add additional libraries to help you with this
 import numpy as np
+from PIL import Image
+
+
+def img2matr(file_name: str):
+    image = Image.open(file_name)  # Open the image
+    grayscale_image = image.convert("L")  # Convert the image to grayscale
+
+    matrix = np.array(grayscale_image)  # Convert the image into matrix
+
+    return matrix
+
+
