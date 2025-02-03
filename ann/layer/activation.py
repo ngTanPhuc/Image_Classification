5a8b9892a415f94d144efa1e9d0af78dc38e0537
Nguyễn Tấn Phúc <99126475+JackWolffang@users.noreply.github.com>
2025-02-03 15:20:10 +0700

    comments and parameters update
    update comments of ILayer.py and activation.py. Update the parameters in activation.py

--- ann/layer/activation.py
+++ ann/layer/activation.py
@@ -1,13 +1,32 @@
+"""
+The activation function f will calculate y* to determine whether the neuron should be activated (on/off) or not
+y*_1 = f(y_1)
+y*_2 = f(y_2)
+.
+.
+.
+y*_j = f(y_j)
+
+         X  --->  +------------------+  ------->   Y   ----->   +------------------+  ---> Y*
+                  |      Layer       |                          | Activation Layer |
+                  |        W         |                          |                  |
+     dE/dX  <---  +------------------+  <-----  dE/dY  <-----   +------------------+  <--- dE/dY*
+                          |
+                          v
+                        dE/dW
+"""
+
+
 import numpy as np
 from ILayer import ILayer
 
 
 class Activation(ILayer):
-    def __init__(self):
+    def __init__(self, activation, activation_derivative):
         # TODO
         super().__init__()
 
-    def forward(self, input):
+    def forward(self, X):
         # TODO
         pass
 
